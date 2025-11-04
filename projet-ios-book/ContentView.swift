4d659bee999250807ed143b61bf173eeb62131c9
MARIE-MAGDELAINE Kyllian <km253685@cm7tm29mhv.edu.atlas.local>
2025-11-04 14:09:09 +0100

    Project initialization

---
+++ projet-ios-book/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projet-ios-book
+//
+//  Created by MARIE-MAGDELAINE Kyllian on 04/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projet-ios-book/projet_ios_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  projet_ios_bookApp.swift
+//  projet-ios-book
+//
+//  Created by MARIE-MAGDELAINE Kyllian on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct projet_ios_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
