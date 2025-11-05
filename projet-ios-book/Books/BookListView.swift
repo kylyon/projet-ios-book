5be81663224dad19fa4136a1ac542a366fdf3727
MARIE-MAGDELAINE Kyllian <km253685@cm7tm29mhv.edu.atlas.local>
2025-11-05 14:27:58 +0100

    fix: HomeView with component

--- projet-ios-book/Books/BookListView.swift
+++ projet-ios-book/Books/BookListView.swift
@@ -22,32 +22,7 @@ struct BookListView: View {
                
                     
                     List(viewModel.researchBook()) { book in
-<<<<<<< HEAD
-                       HStack {
-                           Image(book.coverImage).resizable().frame(width: 50, height: 70)
-                            Text(book.title)
-                            Spacer()
-                       }
-                       .badge("\(book.author)")
-                       .swipeActions(edge: .leading){
-                           
-                           //Share data...
-                           ShareLink(item: URL(string: "https://developer.apple.com/xcode/swiftui/")!) {
-                               Label("Share", systemImage: "square.and.arrow.up")
-                           }
-                          
-                           .tint(.orange)
-                           
-                           //Delete Data...
-                           Button("Delete", systemImage: "delete.left.fill",){
-                               viewModel.deleteIsOpen = true
-                           }.tint(.red)
-                       }
-                       .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
-                       
-=======
                         BookItem(BookItemData(title: book.title, coverImage: book.coverImage, author: book.author))
->>>>>>> ac9ec7f (added: Component BookItem, BookCard)
                     }
                     .navigationTitle(Text("Book List"))
                     .searchable(text: $viewModel.searchText)
